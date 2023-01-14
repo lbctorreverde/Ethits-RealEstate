1c84cc4d728a2d0016a25c901ad47d5303222b91
Leon Bryan <lbctorreverde@bpsu.edu.ph>
2023-01-14 18:53:58 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    include_once 'header.php';
+?>
+
+    <section class="intro-intro d-flex flex-column text-center justify-content-center align-items-center" id="intro-one">
+        <h1>Find the Agent of your needs </h1>
+        <h3>Hero subtitle example, <br> replace with something remarkable</h3>
+    </section>
+
+    <section class="intro-intro d-flex container-fluid flex-column justify-content-center" id="intro-two">
+        <h1>"Okay honestly, what do you guys do?"</h1>
+        <h3>Hero subtitle example, replace with something remarkable</h3>
+    </section>
+
+    <section class="intro-intro d-flex container-fluid text-end flex-column justify-content-center" id="intro-three">
+        <h1>A roster of Real Estate Agents at your fingertips</h1>
+        <h3>Hero subtitle example, replace with something remarkable</h3>
+    </section>
+
+</body>
+
+</html>
