66fcbd5db8cfcbd5a7ea7d542d50b6f20ccdec0c
Leon Bryan <lbctorreverde@bpsu.edu.ph>
2023-01-16 23:13:07 +0800

    Added Edit Profile Pages

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     include_once 'header.php';
-?>
 
+?>
     <section class="intro-intro d-flex flex-column text-center justify-content-center align-items-center" id="intro-one">
         <h1>Find the Agent of your needs </h1>
         <h3>Hero subtitle example, <br> replace with something remarkable</h3>
