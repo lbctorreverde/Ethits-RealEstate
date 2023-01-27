1f4c82f3445aa65e4eeb47d62348d562c8d8b029
Leon Bryan <lbctorreverde@bpsu.edu.ph>
2023-01-27 20:53:51 +0800

    Added animations for index.html

--- index.php
+++ index.php
@@ -6,25 +6,30 @@ include_once 'header.php';
     <?php include 'css/index.css' ?>
 </style>
 
-
-
-
-
 <section class="intro-intro d-flex flex-column text-center justify-content-center align-items-center" id="intro-one">
-    <h1>Find the Agent for your needs </h1>
-    <h3 class="lead">Looking for an agent? <br>You've come to the right place!</h3>
+    <div class="hidden">
+        <h1>Find the Agent for your needs </h1>
+        <h3 class="lead">Looking for an agent? <br>You've come to the right place!</h3>
+    </div>
 </section>
 
 <section class="intro-intro d-flex container-fluid flex-column justify-content-center ps-5" id="intro-two">
-    <h1>"First of all, what does this thing do?"</h1>
-    <h3 class="lead">This "thing" aims to assist not just Real Estate agents, but also people looking for agents or properties regarding their Real Estate needs.</h3>
+    <div class="hidden">
+        <h1>"First of all, what does this thing do?"</h1>
+        <h3 class="lead">This "thing" aims to assist not just Real Estate agents, but also people looking for agents or properties regarding their Real Estate needs.</h3>
+    </div>
 </section>
 
 <section class="intro-intro d-flex container-fluid text-end flex-column justify-content-center pe-5" id="intro-three">
-    <h1>A roster of Real Estate Agents at your fingertips</h1>
-    <h3 class="lead">Or maybe you're an agent who wants to be heard and attract more clients.</h3>
+    <div class="hidden">
+        <h1>A roster of Real Estate Agents at your fingertips</h1>
+        <h3 class="lead">Or maybe you're an agent who wants to be heard and attract more clients.</h3>
+    </div>
 </section>
 
+<script>
+    <?php require_once 'js/index.js' ?>
+</script>
 
 <?php
 include_once 'footer.php';
