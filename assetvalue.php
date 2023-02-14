f6cf6c38e0fcd7d3f78f3aa4106671d4f37d3908
Leon Bryan <lbctorreverde@bpsu.edu.ph>
2023-02-14 14:19:48 +0800

    Asset value update

--- assetvalue.php
+++ assetvalue.php
@@ -13,27 +13,63 @@ include_once 'header.php';
 </div>
 
 <div class="container-fluid">
-    <div class="d-flex flex-row justify-content-center">
+    <div class="d-flex flex-row justify-content-center ">
         <div class="row mx-2">
-            <div class="d-flex justify-content-center">
-                <div class="card shadow">
-                    <div class="card-body text-center">
+            <div class="d-flex justify-content-center align-content-center">
+                <div class="card shadow rounded-0">
+                    <div class="card-body">
                         <form>
-                        <div class="row gy-2">
-                            <div class="col-6">
-                                <label for="" class="form-label">Square Footage (m²)</label>
-                                <input type="number" class="form-control" aria-label="Square Footage">
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label for="" class="form-label">Square Footage (m²):</label>
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" aria-label="Square Footage" value="0">
+                                </div>
                             </div>
-                            <div class="col-6">
-                                <label for="" class="form-label">Lot size (m²)</label>
-                                <input type="number" class="form-control" aria-label="Lot size">
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label for="" class="form-label">Lot size (m²):</label>
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" aria-label="Lot size" value="0">
+                                </div>
                             </div>
-                            <div class="d-flex flex-column text-center justify-content-center align-items-center">
-                                <label for="exampleInputEmail1" class="form-label">Age (Years)</label>
-                                <input type="number" class="form-control w-25" aria-label="First name">
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label for="" class="form-label">Age (Years):</label>
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" aria-label="First name" value="0">
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <label for="disabledSelect" class="form-label">Municipality</label>
+
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label for="" class="form-label">Garages:</label>
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" aria-label="Square Footage" value="0">
+                                </div>
+                            </div>
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label for="" class="form-label">Bedrooms:</label>
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" aria-label="Square Footage" value="0">
+                                </div>
+                            </div>
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label for="" class="form-label">Bathrooms:</label>
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" aria-label="Square Footage" value="0">
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="municipality">
                                 <select id="disabledSelect" class="form-select">
                                     <option selected>Select Municipality</option>
                                     <option>Abucay</option>
@@ -50,25 +86,8 @@ include_once 'header.php';
                                     <option>Samal</option>
                                 </select>
                             </div>
-                        </div>
-                        <hr>
-                        <span>Number of: </span>
-                        <div class="row">
-                            <div class="col">
-                                <label for="" class="form-label">Garages</label>
-                                <input type="number" class="form-control" aria-label="Square Footage">
-                            </div>
-                            <div class="col">
-                                <label for="" class="form-label">Bedrooms</label>
-                                <input type="number" class="form-control" aria-label="Square Footage">
-                            </div>
-                            <div class="col">
-                                <label for="" class="form-label">Bathrooms</label>
-                                <input type="number" class="form-control" aria-label="Square Footage">
-                            </div>
-                        </div>
-                        <hr>
-                        <button type="button" class="btn btn-outline-light w-100">Calculate</button>
+                            <hr>
+                            <button type="button" class="btn btn-dark w-100">Calculate</button>
                         </form>
                     </div>
 
